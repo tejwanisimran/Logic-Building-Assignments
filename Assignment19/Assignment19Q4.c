f1625daa2c4b01f5ee2c6def0897dd7a1d1bfc98
tejwanisimran <tejwanisimran1802@gmail.com>
2025-05-30 00:12:04 +0530

    Add files via upload

---
+++ Assignment19/Assignment19Q4.c
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Description : Accept N numbers from user and rteurn frequency of 11 from it
+/*
+    Input :  N: 6
+    Elements : 85 66 11 80 93 88 
+    Output : 1
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int Arr[],int iLength)
+{
+    int iCnt = 0,iCount = 0 ;
+    
+    for(iCnt = 0;iCnt < iLength ; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            iCount++;
+        }
+        
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iSize = 0,iCnt = 0,iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(NULL == p)
+    {
+        printf("Unable to allocate the memory");
+        return -1;
+    }
+    printf(" Enter %d elements : ",iSize);
+
+    for(iCnt = 0; iCnt < iSize ; iCnt++)
+    {
+        printf("Enter element : %d :",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+        iRet =  Frequency(p,iSize);
+
+        printf("Frequency of 11 is : %d",iRet);
+
+    free(p);
+
+    return 0;
+}
